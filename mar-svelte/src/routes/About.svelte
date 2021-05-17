be842dbafb7c465e4eb7af2338b72a7003f040ff
Jesús Sánchez Cuadrado <jesus.sanchez.cuadrado@gmail.com>
2021-05-17 12:31:42 +0200

    UI: Rework a bit the About page

--- mar-svelte/src/routes/About.svelte
+++ mar-svelte/src/routes/About.svelte
@@ -12,29 +12,6 @@
 
 <p>MAR is a search engine specifically designed for models.</p>
 
-<!-- <p>
-It is still a prototype, so you can expect changes in the web interface, the REST API. 
-Our aim is that eventually it becomes an stable and useful tool for the modellin
-g community.
-</p> -->
-
-<h4>Updates</h4>
-<ul>
-<li>2020-02-22: Major version released</li>
-<li>2020-06-06: UI improvement. Links to models when available.</li>
-<li>2020-05-29: Upgraded server. Indexed around 50.000 Ecore, UML2 and BPMN2 models.</li>
-<li>2020-05-22: Initial prototype. Running in a slow server.</li>
-</ul>
-
-<!--
-<h4>Roadmap</h4>
-<ul>
-<li>Index more models. In the short term: more UML2 and Archimate</li>
-<li>Improve the search result page to make it more useful</li>
-<li>Include textual and graphical editor for building the queries</li>
-</ul>
--->
-
 <h4>Authors</h4>
 <ul>
     <li><a href="mailto:joseantonio.hernandez6@um.es">José Antonio Hernández López</a></li>
@@ -43,7 +20,7 @@ g community.
 
 <h4>Source code</h4>
 <p>
-    Check it out at <a href="http://github.com/mar-plataform/mar">GitHub</a>
+    Check it out at <a href="http://github.com/mar-platform/mar">GitHub</a>
 </p>
 
 <h4>Research</h4>
