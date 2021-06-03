634d1c2abdf4721be65463d052f2124316a46cfa
Jesús Sánchez Cuadrado <jesus.sanchez.cuadrado@gmail.com>
2021-06-03 11:11:57 +0200

    Xtext: Crawler for Xtext

---
+++ mar-crawlers/github/crawl-xtext.py
@@ -0,0 +1,13 @@
+import csv
+import os
+import github_crawler as gc
+
+if __name__ == "__main__":
+    args = gc.parse_args()
+    
+    print("Generating in ", args.output)
+    print("  with initial=", args.init)
+    
+    with open(os.path.join(args.output, 'xtext_download.csv'), 'a+', newline='') as file:
+        writer = csv.writer(file)
+        gc.process('grammar', 'xtext', writer, args.output, args.init, 30_000_000, args.step)
