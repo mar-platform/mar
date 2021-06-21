24a0e8d78842d40fc1c2878961cf3603bb8134fe
Jesús Sánchez Cuadrado <jesus.sanchez.cuadrado@gmail.com>
2021-06-21 23:05:48 +0200

    Links to the raw models

--- mar-svelte/src/routes/Status.svelte
+++ mar-svelte/src/routes/Status.svelte
@@ -34,3 +34,25 @@
     </ul>    
 {/if}
 
+<h4>Raw models</h4>
+<p>
+    The following packages contain the raw models crawled by MAR. It should be ok to use these models for research purposes (and even better if you cite our MAR papers!).
+    You can also checkout <a href="http://modelset.github.io">ModelSet</a> which is a curated and labelled dataset of models.
+</p>
+<ul>    
+    <li><a href="http://sanchezcuadrado.es/files/raw/repo-github-ecore.tar.gz">Ecore - GitHub</a></li>
+    <li><a href="http://sanchezcuadrado.es/files/raw/repo-atlanmod-ecore.tar.gz">Ecore - Atlanmod</a></li>
+    <li><a href="http://sanchezcuadrado.es/files/raw/repo-genmymodel-ecore.tar.gz">Ecore - GenMyModel</a></li>
+    <li><a href="http://sanchezcuadrado.es/files/raw/repo-genmymodel-archimate.tar.gz">Archimate - GenMyModel</a></li>
+    <li><a href="http://sanchezcuadrado.es/files/raw/repo-github-archimate.tar.gz">Archimate - GitHub</a></li>
+    <li><a href="http://sanchezcuadrado.es/files/raw/repo-genmymodel-bpmn2.tar.gz">BPMN2 - GenMyModel</a></li>
+    <li><a href="http://sanchezcuadrado.es/files/raw/repo-genmymodel-rds.tar.gz">RDS - GenMyModel</a></li>
+    <li><a href="http://sanchezcuadrado.es/files/raw/repo-genmymodel-uml.tar.gz">UML - GenMyModel</a></li>
+    <li><a href="http://sanchezcuadrado.es/files/raw/repo-github-pnml.tar.gz">PNML - GitHub</a></li>
+    <li><a href="http://sanchezcuadrado.es/files/raw/repo-github-sculptor.tar.gz">Sculptor - GitHub</a></li>
+    <li><a href="http://sanchezcuadrado.es/files/raw/repo-github-uml.tar.gz">UML - GitHub</a></li>
+    <li><a href="http://sanchezcuadrado.es/files/raw/repo-github-xtext.tar.gz">Xtext - GitHub</a></li>
+    <!--
+    <li><a href=" repo-simulink-emf.tar.gz">Simulink</a></li>
+    -->
+</ul>
