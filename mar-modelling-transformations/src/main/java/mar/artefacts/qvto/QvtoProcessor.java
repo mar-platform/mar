534193922eac36b52069600f2e059faa259a93f8
Jesús Sánchez Cuadrado <jesus.sanchez.cuadrado@gmail.com>
2022-02-15 11:18:45 +0100

    Transformation loading improvements

--- mar-modelling-transformations/src/main/java/mar/artefacts/qvto/QvtoProcessor.java
+++ mar-modelling-transformations/src/main/java/mar/artefacts/qvto/QvtoProcessor.java
@@ -8,15 +8,9 @@ import java.util.Set;
 
 import javax.annotation.Nonnull;
 
-import org.eclipse.core.runtime.NullProgressMonitor;
-import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EPackage;
-import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
-import org.eclipse.m2m.internal.qvt.oml.InternalTransformationExecutor;
-import org.eclipse.m2m.internal.qvt.oml.QvtMessage;
-import org.eclipse.m2m.internal.qvt.oml.compiler.CompiledUnit;
 
 import mar.analysis.ecore.EcoreRepository;
 import mar.analysis.ecore.EcoreRepository.EcoreModel;
@@ -33,46 +27,28 @@ public class QvtoProcessor {
 	}
 	
 	public Transformation load(@Nonnull String qvtoFile) {
-		Collection<String> missingMetamodels = getMissingMetamodels(qvtoFile);
+		Qvto tmp = new QvtoLoader().load(qvtoFile, Collections.emptyList());
+		Collection<String> expectedMetamodels = tmp.getMetamodelURIs(); 
+		
+		// Collection<String> missingMetamodels = getMissingMetamodels(qvtoFile);
 		
 		Set<EcoreModel> metamodels = new HashSet<EcoreRepository.EcoreModel>();
 		Set<EPackage> packages = new HashSet<EPackage>();
 		ResourceSet rs = new ResourceSetImpl();
-		for (String uri : missingMetamodels) {
+		for (String uri : expectedMetamodels) {
 			List<EcoreModel> m = this.repo.findEcoreByURI(uri);
 			if (! m.isEmpty()) {
 				EcoreModel ecore = m.get(0);
 				metamodels.add(ecore);
-				
-				// TODO: Factorise URIExtractor combinator generator to try all possible combinations
-				Resource r = ecore.load(rs);
-				r.getAllContents().forEachRemaining(o -> {
-					if (o instanceof EPackage) {
-						EPackage pkg = (EPackage) o;
-						packages.add(pkg);						
-					}
-				});
+				packages.addAll(ecore.getPackages(rs));
 			}
 		}
 		
 		Qvto trafo = new QvtoLoader().load(qvtoFile, packages);
+		
 		trafo.addMetamodels(metamodels);
-		
-//		System.out.println("Loaded " + qvtoFile + " with " + unit.getErrors().size() + " errors");
-//		int i = 0;
-//		for (QvtMessage qvtMessage : unit.getErrors()) {
-//			System.out.println("  - " + qvtMessage);
-//			if (i++ > 5)
-//				break;
-//		}
-		
-		return trafo;
-	}
 	
-	@Nonnull
-	private Collection<String> getMissingMetamodels(@Nonnull String qvtoFile) {
-		Qvto trafo = new QvtoLoader().load(qvtoFile, Collections.emptyList());
-		return trafo.getMissingMetamodels();
+		return trafo;
 	}
 	
 }
