a70d5855b6f841dc297af4f88accda0195fa638a
Jesús Sánchez Cuadrado <jesus.sanchez.cuadrado@gmail.com>
2022-02-27 10:49:44 +0100

    RecoveryGraph as abstraction to gather loose information about a modelling artefact. Support for recovering information from Epsilon build files.

---
+++ mar-modelling-transformations/src/main/java/mar/artefacts/qvto/QvtoInspector.java
@@ -0,0 +1,44 @@
+package mar.artefacts.qvto;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.util.Collection;
+import java.util.Collections;
+
+import mar.artefacts.Metamodel;
+import mar.artefacts.ProjectInspector;
+import mar.artefacts.RecoveredPath;
+import mar.artefacts.Transformation.Qvto;
+import mar.artefacts.graph.RecoveryGraph;
+
+public class QvtoInspector extends ProjectInspector {
+
+	//private final EcoreRepository ecoreRepository;
+
+	public QvtoInspector(Path repoFolder, Path projectPath /* , AnalysisDB metamodels */) {
+		super(repoFolder, projectPath);
+		//this.ecoreRepository = new EcoreRepository(metamodels, repoFolder.toFile());
+	}
+
+	@Override
+	public RecoveryGraph process(File qvtoFile) throws Exception {
+		Qvto tmp = new QvtoLoader().load(qvtoFile.getAbsolutePath(), Collections.emptyList());
+		Collection<String> expectedMetamodels = tmp.getMetamodelURIs();
+		
+		QvtoProgram program = new QvtoProgram(new RecoveredPath(qvtoFile.toPath()));
+		
+		RecoveryGraph graph = new RecoveryGraph();
+		graph.addProgram(program);
+		
+		for (String uri : expectedMetamodels) {
+			// Do I have a way to extract the logical name of the meta-model from QVTo file?
+			Metamodel mm = new Metamodel(uri);
+			mm.setURI(uri);
+			graph.addMetamodel(mm);
+			program.addMetamodel(mm);
+		}
+		
+		return graph;
+	}
+
+}
---
+++ mar-modelling-transformations/src/main/java/mar/artefacts/qvto/QvtoProgram.java
@@ -0,0 +1,14 @@
+package mar.artefacts.qvto;
+
+import javax.annotation.Nonnull;
+
+import mar.artefacts.FileProgram;
+import mar.artefacts.RecoveredPath;
+
+public class QvtoProgram extends FileProgram {
+
+	public QvtoProgram(@Nonnull RecoveredPath path) {
+		super(path);
+	}
+
+}
