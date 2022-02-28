0820e68d445d8526000cf3bc610a0bbb336cef03
Jesús Sánchez Cuadrado <jesus.sanchez.cuadrado@gmail.com>
2022-02-28 13:47:30 +0100

    Add Xtext inspector

---
+++ mar-analysis-backend/src/main/java/mar/analysis/backend/megamodel/XtextInspector.java
@@ -0,0 +1,54 @@
+package mar.analysis.backend.megamodel;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.util.function.Function;
+
+import mar.artefacts.FileProgram;
+import mar.artefacts.Metamodel;
+import mar.artefacts.MetamodelReference;
+import mar.artefacts.ProjectInspector;
+import mar.artefacts.RecoveredPath;
+import mar.artefacts.graph.RecoveryGraph;
+import mar.models.xtext.XtextLoader;
+import mar.models.xtext.XtextLoader.XtextAnalysisResult;
+
+public class XtextInspector extends ProjectInspector {
+
+	public XtextInspector(Path repoFolder, Path projectSubPath) {
+		super(repoFolder, projectSubPath);
+	}
+
+	@Override
+	public RecoveryGraph process(File f) throws Exception {
+		XtextLoader loader = new XtextLoader();
+		XtextAnalysisResult r = loader.doAnalysis(f);
+		
+		RecoveryGraph graph = new RecoveryGraph();
+		
+		XtextProgram p = new XtextProgram(new RecoveredPath(f.toPath()));		
+		graph.addProgram(p);
+		
+		for (String uri : r.getGeneratedURIs()) {
+			Metamodel mm = new Metamodel(uri);
+			graph.addMetamodel(mm);
+			p.addMetamodel(mm, MetamodelReference.Kind.GENERATE, MetamodelReference.Kind.TYPED_BY);
+		}
+				
+		for (String uri : r.getImportedURIs()) {
+			Metamodel mm = new Metamodel(uri);
+			graph.addMetamodel(mm);
+			p.addMetamodel(mm, MetamodelReference.Kind.IMPORT, MetamodelReference.Kind.TYPED_BY);
+		}
+		
+		return graph;
+	}	
+	
+	public static class XtextProgram extends FileProgram {
+
+		public XtextProgram(RecoveredPath path) {
+			super(path);
+		}
+		
+	}
+}
