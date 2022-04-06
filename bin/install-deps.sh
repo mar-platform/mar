5216b035cb85525c20b720259a5d622ccb990c7c
Jesús Sánchez Cuadrado <jesus.sanchez.cuadrado@gmail.com>
2022-04-06 13:23:40 +0200

    Install AnATLyzer in .m2 to fix poms

--- bin/install-deps.sh
+++ bin/install-deps.sh
@@ -5,6 +5,8 @@ mvn install:install-file -Dfile=thirdparty/archimate/com.archimatetool.jdom.jar 
 
 mvn install:install-file -Dfile=thirdparty/lilypond/org.elysium_0.6.0.201805301416.jar -DgroupId=elysium -DartifactId=elysium -Dversion=0.6.0 -Dpackaging=jar
 
+mvn install:install-file -Dfile=thirdparty/anatlyzer/all.jar -DgroupId=anatlyzer -DartifactId=anatlyzer-bundle -Dversion=0.8.0 -Dpackaging=jar
+
 #    jaxen-1.1.3.jar  jdom-2.0.5.jar
 
 # cp thirdparty/simulink/simulink.ecore mar-modelling-eclipse/src/main/java/mar/models/simulink
