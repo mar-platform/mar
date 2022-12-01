f37adb57c02072e9afd3dfa96ab9e42fb6b785ae
Jesús Sánchez Cuadrado <jesusc@um.es>
2022-12-01 16:27:59 +0100

    Improve Acceleo metamodel URI parsing

--- mar-modelling-transformations/src/main/java/mar/artefacts/acceleo/AcceleoInspector.java
+++ mar-modelling-transformations/src/main/java/mar/artefacts/acceleo/AcceleoInspector.java
@@ -1,13 +1,14 @@
 package mar.artefacts.acceleo;
 
+import java.io.BufferedReader;
 import java.io.File;
-import java.nio.file.Files;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import com.google.common.annotations.VisibleForTesting;
 
@@ -31,14 +32,7 @@ public class AcceleoInspector extends ProjectInspector {
 	 */
 	@Override
 	public RecoveryGraph process(File f) throws Exception {
-	    List<String> lines = Files.readAllLines(f.toPath());
-	    List<String> uris = Collections.emptyList();
-	    for (String line : lines) {
-	    	uris = getURIs(line);
-	    	if (! uris.isEmpty()) {
-	    		break;
-	    	}
-		}
+	    List<String> uris = getURIs(f);
 
 	    if (uris.isEmpty())
 	    	return null;
@@ -56,42 +50,99 @@ public class AcceleoInspector extends ProjectInspector {
 		
 		return graph;
 	}
-
-	private static Pattern pattern = Pattern.compile("^\\[\\s*module");
+	
+	private static List<String> getURIs(File f) throws IOException {
+		return getURIs(new FileReader(f));
+	}
 	
 	@VisibleForTesting
-	static List<String> getURIs(String line) {
-		line = line.stripLeading();
-		Matcher matcher = pattern.matcher(line);
-		
-		if (! matcher.find())
-			return Collections.emptyList();
-		
-		int start = line.indexOf("(");
-		if (start == -1)
-			return Collections.emptyList();
-		
-		int end   = line.lastIndexOf(")");
-		if (end == -1)
-			return Collections.emptyList();;
-		
-		
-		List<String> uris = new ArrayList<String>();
-		String[] parts = line.substring(start + 1, end).split(",");
-		for (String part : parts) {
-			part = part.trim();
-			int startSubstr = 0;
-			int endSubstr = part.length();
-			if (part.startsWith("'") || part.startsWith("\""))
-				startSubstr = 1;
-			if (part.endsWith("'") || part.endsWith("\""))
-				endSubstr = part.length() - 1;
-			
-			String uri = part.substring(startSubstr, endSubstr);
-			uris.add(uri);
+	protected static List<String> getURIs(Reader reader) throws IOException {
+		String transformationName = null;
+		List<String> uris = new ArrayList<>();
+		try(BufferedReader br = new BufferedReader(reader)) {
+			int c;
+			MAIN: while((c = br.read()) != -1) {
+				char character = (char) c;
+				if (character == '[') {
+					c = skipWhitespace(br);
+					if (((char) c) != 'm')
+						continue MAIN;
+					
+					char[] buffer = new char[5];
+					int read = br.read(buffer);
+					if (read != 5 || ! "odule".equals(new String(buffer))) {
+						continue MAIN;
+					}
+					
+					c = skipWhitespace(br);
+
+					// Read until "("
+					StringBuilder name = new StringBuilder();
+					while(c != -1) {
+						if (((char) c) == '(') {
+							break;
+						} else {
+							name.append((char) c);
+						}
+						c = br.read();
+					}
+					
+					if (c == -1)
+						return Collections.emptyList();
+					
+					transformationName = name.toString();
+					
+					while (true) {
+						c = skipWhitespace(br);
+
+						// This should be '
+						if (c == -1 || ((char) c) != '\'')
+							return Collections.emptyList();
+	
+						// Read everything until next '
+						StringBuilder uri = new StringBuilder();
+						while((c = br.read()) != -1) {
+							if (c == '\'') {
+								break;
+							} else {
+								uri.append((char) c);
+							}
+						}			
+						
+						if (c == -1)
+							return Collections.emptyList();
+					
+						uris.add(uri.toString());
+						c = skipWhitespace(br);
+	
+						if (c == -1)
+							return Collections.emptyList();
+						else if (((char) c) == ')')
+							break MAIN;
+						else if (((char) c) == ',')
+							continue;
+						else
+							return Collections.emptyList();
+						
+					}
+					
+				}
+
+			}
 		}
-		
+
 		return uris;
 	}
+	
+	private static int skipWhitespace(BufferedReader br) throws IOException {
+		int c;
+		while((c = br.read()) != -1) {
+			if (! Character.isWhitespace(c)) {
+				return c;
+			}
+		}
+		return -1;
+	}
+
 	
 }
