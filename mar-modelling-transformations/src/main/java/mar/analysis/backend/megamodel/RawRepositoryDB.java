d306bda7f722601da6c52b52311e818b4c46b00d
Jesús Sánchez Cuadrado <jesus.sanchez.cuadrado@gmail.com>
2023-03-27 20:33:13 +0200

    Update EMF and Xtext versions for better compatibility and add ResultAnalyser to inspect analysis results

--- mar-modelling-transformations/src/main/java/mar/analysis/backend/megamodel/RawRepositoryDB.java
+++ mar-modelling-transformations/src/main/java/mar/analysis/backend/megamodel/RawRepositoryDB.java
@@ -7,6 +7,8 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.annotation.Nonnull;
 
@@ -41,6 +43,21 @@ public class RawRepositoryDB implements AutoCloseable {
 		}
 	}
 	
+	public List<RawFile> getFiles() throws SQLException {
+		PreparedStatement files = connection.prepareStatement("select project_path, file_path, extension, type from files");
+		List<RawFile> results = new ArrayList<>();
+		ResultSet rs = files.executeQuery();
+		while (rs.next()) {
+			String project = rs.getString(1);
+			String filepath = rs.getString(2);
+			String extension = rs.getString(3);
+			String type = rs.getString(4);
+			results.add(new RawFile(project, filepath, extension, type));
+		}
+		
+		return results;
+	}
+		
 	public RawRepositoryStats getStats() {
 		try {
 			PreparedStatement artefactCount = connection.prepareStatement("select type, count(*) from files group by type");
@@ -62,5 +79,36 @@ public class RawRepositoryDB implements AutoCloseable {
 	public static String getConnectionString(File file) {
 		return "jdbc:sqlite:" + file.getAbsolutePath();
 	}
-	
+
+	public static class RawFile {
+
+		private final String project;
+		private final String filepath;
+		private final String extension;
+		private final String type;
+
+		public RawFile(String project, String filepath, String extension, String type) {
+			this.project = project;
+			this.filepath = filepath;
+			this.extension = extension;
+			this.type = type;
+		}
+		
+		public String getProject() {
+			return project;
+		}
+		
+		public String getFilepath() {
+			return filepath;
+		}
+		
+		public String getType() {
+			return type;
+		}
+		
+		public String getExtension() {
+			return extension;
+		}		
+	}
+
 }
