cd0df7c87129efd1a503d10e682f28eb028ef588
Jesús Sánchez Cuadrado <jesus.sanchez.cuadrado@gmail.com>
2024-04-06 17:53:50 +0200

    Scripts for indexing with JVector and SQLite

---
+++ scripts/indexing/index-jvector.sh
@@ -0,0 +1,28 @@
+RUN_COMMAND="Exec with $0 <json-config> <configuration-name> "
+
+JSON=$1
+NAME=$2
+
+if [ -z "$REPO_MAR" ]
+  then
+      echo "Please define REPO_MAR"
+      exit
+fi
+
+if [ -z "$JSON" ]
+  then
+      echo $RUN_COMMAND
+      exit
+fi
+
+
+if [ -z "$NAME" ]
+  then
+      echo $RUN_COMMAND
+      exit
+fi
+
+#java -jar $REPO_MAR/mar-indexer-embeddings/target/mar-indexer-embeddings-1.0-SNAPSHOT-jar-with-dependencies.jar $JSON -t $NAME -m full
+
+
+java -cp $REPO_MAR/mar-indexer-embeddings/target/mar-indexer-embeddings-1.0-SNAPSHOT-jar-with-dependencies.jar mar.indexer.embeddings.CreateIndex $JSON $INDEX_TARGET/jvector/$NAME.jvector $INDEX_TARGET/jvector/$NAME.info -t $NAME
