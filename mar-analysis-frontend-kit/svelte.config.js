699755d66ad924d0cc60656c497d85a9ac1cdfa8
Antonio Martínez <antoniom220502@gmail.com>
2026-03-23 12:50:41 +0100

    Initial sveltekit project

---
+++ mar-analysis-frontend-kit/svelte.config.js
@@ -0,0 +1,14 @@
+import adapter from '@sveltejs/adapter-static';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: { 
+		adapter: adapter(),
+	},
+	vitePlugin: {
+		dynamicCompileOptions: ({ filename }) =>
+			filename.includes('node_modules') ? undefined : { runes: true }
+	}
+};
+
+export default config;
